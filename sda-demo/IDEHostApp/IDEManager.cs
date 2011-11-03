3b800b90b3af996db3df12aabd66f2d2f7b76262
denis <dennisyolkin@gmail.com>
2011-11-03 10:52:56 +0000

    resolve assembly wip

--- sda-demo/IDEHostApp/IDEManager.cs
+++ sda-demo/IDEHostApp/IDEManager.cs
@@ -8,125 +8,135 @@ using ICSharpCode.SharpDevelop.Sda;
 
 namespace IDEHostApp
 {
-	class IDEManager
-	{
-		private string baseDir;
-		private string binDir;
-		private string dataDir;
-		private string addInDir;
+   class IDEManager
+   {
+      private string baseDir;
+      private string binDir;
+      private string dataDir;
+      private string addInDir;
 
-		private WorkbenchSettings workbenchSettings;
-		private SharpDevelopHost host;
-		private StartupSettings startup;
+      private WorkbenchSettings workbenchSettings;
+      private SharpDevelopHost host;
+      private StartupSettings startup;
 
-		public IDEManager()
-		{
-			baseDir = @"C:\Program Files\SharpDevelop\4.1";
-			binDir = baseDir + @"\bin\";
-			dataDir = baseDir + @"\data\"; 
-			addInDir = baseDir + @"\AddIns\";
+      public IDEManager()
+      {
+         baseDir = @"C:\home\master\sda\SharpDevelop_4.1.0.8000_Source";
+         //baseDir = @"C:\Program Files\SharpDevelop\4.1";
+         binDir = baseDir + @"\bin\";
+         dataDir = baseDir + @"\data\";
+         addInDir = baseDir + @"\AddIns\";
 
-			AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+         AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
 
-			ConfigureEnviorenment();
-		}
+         ConfigureEnviorenment();
+      }
 
-		Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-		{
-			Assembly result = null;
-			if (args != null && !string.IsNullOrEmpty(args.Name))
-			{
-				try
-				{
-					result = Assembly.LoadFrom(FindAssembly(args.Name));
-				}
-				catch (Exception e)
-				{
-					Console.Error.WriteLine("SharpDevelop assembly loading error: " + e.Message);
-				}
-			}
+      private bool suppressResolve;
 
-			return result;
-		}
+      Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+      {
+         if (suppressResolve)
+            return null;
+         Assembly result = null;
+         if (args != null && !string.IsNullOrEmpty(args.Name))
+         {
+            try
+            {
+               var assemblyPath = FindAssembly(args.Name);
+               if (!String.IsNullOrEmpty(assemblyPath))
+                  result = Assembly.LoadFrom(assemblyPath);
+               else
+               {
+                  suppressResolve = true;
+                  string nn = args.Name;
+                 // result = Assembly.Load(nn);
+                  suppressResolve = false;
+               }
 
-		private string FindAssembly(string name)
-		{
-			var typeName = name.Split(new string[] { "," }, StringSplitOptions.None)[0];
-			var assemblyPath = Path.Combine(binDir, string.Format("{0}.dll", typeName));
+            }
+            catch (Exception e)
+            {
+               Console.Error.WriteLine("SharpDevelop assembly loading error: " + e.Message);
+            }
+         }
 
-			var strs = typeName.Split('.');
-			var assemblyName = string.Empty;
+         return result;
+      }
 
-			for (int j = strs.Length - 1; j >= 0; j--)
-			{
-				
-				//assemblyName += strs[j];
-				assemblyPath = Path.Combine(binDir, string.Format("{0}.dll", typeName));
-				if (!File.Exists(assemblyPath))
-				{
-					assemblyPath = Path.ChangeExtension(assemblyPath, "exe");
-					if (!File.Exists(assemblyPath))
-					{
-						var strings = typeName.Split('.');
-						var sb = new StringBuilder();
-						for (int i = 0; i < strings.Length - 1; i++)
-						{
-							sb.Append(strings[i]);
-							sb.Append('.');
-						}
-						typeName = sb.ToString();
-						var assembls = Directory.GetFiles(addInDir, typeName + "*", SearchOption.AllDirectories);
-						if (assembls.Length > 0)
-						{
-							assemblyPath = Path.Combine(Path.GetDirectoryName(assembls[0]), string.Format("{0}dll", typeName));
-							if (!File.Exists(assemblyPath))
-							{
-								typeName += strings[strings.Length - 1];
-								assemblyPath = Path.Combine(Path.GetDirectoryName(assembls[0]), string.Format("{0}.dll", typeName));
-							}
-							else break;
-						}
-					}
-					else break;
-				}
-				else break;
-				typeName = typeName.Substring(0, typeName.IndexOf(strs[j]) - 1);
-				//assemblyName += '.';
-			}
-			//if (!File.Exists(assemblyPath))
-			//{
-			//    var dllBasePath = Path.GetDirectoryName(Application.StartupPath) + Path.DirectorySeparatorChar;
-			//    var dllPath = dllBasePath + assemblyName + ".dll";
-			//    if (File.Exists(dllPath))
-			//        assemblyPath = dllPath;
-			//    else
-			//        assemblyPath = Path.Combine(Path.GetDirectoryName(Application.StartupPath.TrimEnd('\\')), assemblyName + ".dll");
-			//}
-			return assemblyPath;
-		}
+      private string FindAssembly(string name)
+      {
+         var typeName = name.Split(new string[] { "," }, StringSplitOptions.None)[0];
+         var assemblyPath = Path.Combine(binDir, string.Format("{0}.dll", typeName));
 
-		private void ConfigureEnviorenment()
-		{
-			if (host != null) return;
+         var strs = typeName.Split('.');
+         var assemblyName = string.Empty;
 
-			startup = new StartupSettings {AllowUserAddIns = true, DataDirectory = dataDir};
-			startup.AddAddInsFromDirectory(addInDir);
+         for (int j = strs.Length - 1; j >= 0; j--)
+         {
 
-			host = new SharpDevelopHost(AppDomain.CurrentDomain, startup);
+            //assemblyName += strs[j];
+            assemblyPath = Path.Combine(binDir, string.Format("{0}.dll", typeName));
+            if (!File.Exists(assemblyPath))
+            {
+               assemblyPath = Path.ChangeExtension(assemblyPath, "exe");
+               if (!File.Exists(assemblyPath))
+               {
+                  var strings = typeName.Split('.');
+                  var sb = new StringBuilder();
+                  for (int i = 0; i < strings.Length - 1; i++)
+                  {
+                     sb.Append(strings[i]);
+                     sb.Append('.');
+                  }
+                  typeName = sb.ToString();
+                  var assembls = Directory.GetFiles(addInDir, typeName + "*", SearchOption.AllDirectories);
+                  if (assembls.Length > 0)
+                  {
+                     assemblyPath = Path.Combine(Path.GetDirectoryName(assembls[0]), string.Format("{0}dll", typeName));
+                     if (!File.Exists(assemblyPath))
+                     {
+                        typeName += strings[strings.Length - 1];
+                        assemblyPath = Path.Combine(Path.GetDirectoryName(assembls[0]), string.Format("{0}.dll", typeName));
+                     }
+                     else break;
+                  }
+               }
+               else break;
+            }
+            else break;
+            if (typeName.IndexOf(strs[j]) > 0)
+               typeName = typeName.Substring(0, typeName.IndexOf(strs[j]) - 1);
+         }
+         if (!File.Exists(assemblyPath))
+         {
+            assemblyPath = string.Empty;
+         }
+         return assemblyPath;
+      }
 
-			workbenchSettings = new WorkbenchSettings();
+      private void ConfigureEnviorenment()
+      {
+         if (host != null) return;
 
-			RunIDE();
-		}
+         startup = new StartupSettings { AllowUserAddIns = true, DataDirectory = dataDir };
+         startup.AddAddInsFromDirectory(addInDir);
 
-		public void RunIDE()
-		{
-			System.Threading.ThreadPool.QueueUserWorkItem(ThreadedRun);
-		}
+         host = new SharpDevelopHost(AppDomain.CurrentDomain, startup);
 
-		void ThreadedRun(object state)
-		{
-			host.RunWorkbench(workbenchSettings);
-		}
-	}
+         workbenchSettings = new WorkbenchSettings();
+
+         RunIDE();
+      }
+
+      public void RunIDE()
+      {
+         System.Threading.ThreadPool.QueueUserWorkItem(ThreadedRun);
+      }
+
+      void ThreadedRun(object state)
+      {
+         host.RunWorkbench(workbenchSettings);
+      }
+   }
 }

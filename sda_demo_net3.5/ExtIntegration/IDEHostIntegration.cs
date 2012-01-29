495f5fb681b190d8dc9de3c1de55099882dcde28
Anton Ashmarin <aashmarin@gmail.com>
2012-01-29 19:08:01 +0000

    WIP

---
+++ sda_demo_net3.5/ExtIntegration/IDEHostIntegration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using SharpDevelopIDEHost;
+
+namespace ExtIntegration
+{
+	class IDEHostIntegration
+	{
+		
+		private static volatile IDEHostIntegration _instance;
+		private static readonly object SyncRoot = new Object();
+
+		private IDEHostIntegration()
+		{
+		}
+
+		public static IDEHostIntegration Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (SyncRoot)
+					{
+						if (_instance == null)
+							_instance = new IDEHostIntegration();
+					}
+				}
+				return _instance;
+			}
+		}
+
+		private ISDAService m_SDAManipulator;
+
+		private ServiceHost callbackHost;
+		public void InitCallbackPipe()
+		{
+			callbackHost = new ServiceHost(typeof(SDAServiceCallback));
+			NetNamedPipeBinding callbackbinding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None);
+			callbackbinding.ReceiveTimeout = TimeSpan.FromHours(42);
+			callbackbinding.SendTimeout = TimeSpan.FromHours(42);
+			callbackHost.AddServiceEndpoint(typeof(ISDAServiceCallback), callbackbinding, WCFCommunicationService.callbackAddress);
+
+			// ERROR: Not supported in C#: OnErrorStatement
+
+			callbackHost.Open();
+		}
+
+	}
+}

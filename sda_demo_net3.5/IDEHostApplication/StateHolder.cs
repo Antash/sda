ff884e2ccf576e652f01b84609cd8dcf8a18db8d
anton <anton@85543b95-aaac-444e-98be-86e0e0c2a9c3>
2012-02-07 17:06:46 +0000

    Build configuration fixed StateHolder implementation started

---
+++ sda_demo_net3.5/IDEHostApplication/StateHolder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace IDEHostApplication
+{
+	internal class StateHolder
+	{
+		enum IDEHostApplicationState
+		{
+			Suspended,
+			Initializing,
+			Running,
+			Suspending,
+			Fault
+		}
+
+		enum SDWorkbenchWindowState
+		{
+			Closed,
+			Opened,
+			Active,
+			Hiden
+		}
+
+		enum ProjectState
+		{
+			Dirty,
+			Clean,
+			Closed
+		}
+
+		enum ProjectBuildState
+		{
+			NotBuilded,
+			Succeded,
+			Fault
+		}
+
+		enum ProjectExecutionState
+		{
+			Stopped,
+			Running,
+			Paused,
+		}
+
+		private static volatile StateHolder _instance;
+		private static readonly object SyncRoot = new Object();
+
+		public static StateHolder Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					lock (SyncRoot)
+					{
+						if (_instance == null)
+							_instance = new StateHolder();
+					}
+				}
+
+				return _instance;
+			}
+		}
+	}
+}
